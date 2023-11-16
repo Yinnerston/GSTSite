be10b67005d4112d2dd03cfdaa5a22c72ae4178a
Nathan Yin <nathanzjyin@gmail.com>
2023-11-17 04:04:47 +1100

    defined header nav items

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 
+
+/**
+ * Homepage
+People
+Publications
+Useful Links
+Events
+Contact
+
+ * @returns 
+ */
 export default function Header() {
     return (
     <header className="App-header">
-        <p>Hello world</p>
-        <p>Something 1</p>
+        <nav className="headerNav">
+            <div className="headerLogo">
+                <a href="/">
+                    LOGO
+                </a>
+            </div>
+            <div className="headerLinks">
+                <a className="headerAnchor" href="/People">
+                    <p className="headerAnchorText">People</p>
+                </a>
+                <a className="headerAnchor" href="/Publications">
+                    <p className="headerAnchorText">Publications</p>
+                </a>
+                <a className="headerAnchor" href="/Useful-links">
+                    <p className="headerAnchorText">Useful Links</p>
+                </a>
+                <a className="headerAnchor" href="/Events">
+                    <p className="headerAnchorText">Events</p>
+                </a>
+                <a className="headerAnchor" href="/Contact">
+                    <p className="headerAnchorText">Contact</p>
+                </a>
+            </div>
+        </nav>
     </header>
     
     )
